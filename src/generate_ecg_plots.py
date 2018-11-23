a10ca84ac4243fbffca48f0bc3b9c6a51fc079b4
Steven Hicks <shicks1000@gmail.com>
2018-11-23 14:54:03 +0100

    fixed plots

--- src/generate_ecg_plots.py
+++ src/generate_ecg_plots.py
@@ -6,39 +6,58 @@ import argparse
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 
+# from scipy.interpolate import spline
+
 from utils import read_csv
 
 from settings import MEDIANS_PATH, MEDIANS_PLOT_PATH
 
+PLOT_DPI = 500
+PLOT_FORMAT = 'png'
 PLOT_COLORS = [
-    '#5ea3a3',
-    '#fa0559',
-    '#c8e4fe',
-    '#5c848e',
-    '#ff9234',
-    '#fd0054',
-    '#f9ad8d',
-    '#80ac7b',
+    '#000000',
+    '#000000',
+    '#000000',
+    '#000000',
+    '#000000',
+    '#000000',
+    '#000000',
+    '#000000',
 ]
+
+Y_TOP_LIMIT = 3000
+Y_BOTTOM_LIMIT = -3000
 
 def generate_ecg_plot(ecg_data, ecg_id, save_path=None):
 
     if not os.path.exists(save_path):
         os.makedirs(save_path)
+    
+    plt.figure(dpi=PLOT_DPI)
+    plt.axis('off')
 
     for index, row in enumerate(ecg_data):
-        plt.plot(range(len(row)), row, color=PLOT_COLORS[index])
 
-    plt.axis('off')
-    plt.savefig(os.path.join(save_path, f'{ ecg_id }.png'), format='png')
+        x = range(len(row))
+        y = row
+
+        # x_smooth = np.linspace(min(x), max(x), 600)
+        # y_smooth = spline(x, y, x_smooth)
+        
+        plt.plot(x, y, color=PLOT_COLORS[index], linewidth=0.1)
+
+    plt.ylim(Y_BOTTOM_LIMIT, Y_TOP_LIMIT)
+    plt.savefig(os.path.join(save_path, f'{ ecg_id }.{ PLOT_FORMAT }'), format=PLOT_FORMAT)
     plt.clf()
+    plt.cla()
+    plt.close()
 
 if __name__ == '__main__':
 
-    median_files = os.listdir(MEDIANS_PATH)
+    median_files = sorted(os.listdir(MEDIANS_PATH))
 
     for index, ecg_file in enumerate(median_files):
-        print(f'Generating features: { index + 1 } / { len(median_files) }', end='\r')
+        print(f'Generating plots: { index + 1 } / { len(median_files) }', end='\r')
 
         ecg_data = read_csv(
             os.path.join(MEDIANS_PATH, ecg_file),
